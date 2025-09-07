c2cc78cc398e86377f7eaf95380e021ecf45f82f
anj-gurjar <anjalikgurjar@gmail.com>
2025-09-07 16:43:34 +0530

    error

--- app/server.ts
+++ app/server.ts
@@ -1,9 +1,8 @@
 import { showRoutes } from 'hono/dev'
 import { createApp } from 'honox/server'
-import * as db from './database/index'
 
 const app = createApp()
 
 showRoutes(app)
-db.connect()
+
 export default app
